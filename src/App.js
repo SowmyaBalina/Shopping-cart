ea168c838b1393bfe1d10caf8b4fa63b81fc2b21
Sowmya Balina <sowmyabalina2329@gmail.com>
2026-03-04 19:26:03 +0530

    Added Context

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 import { useState } from 'react';
 import Items from './components/Items';
 import Navbar from './components/Navbar';
-
+import itemContext from './ItemContext';
 
 function App() {
-  const [total, setTotal] = useState(0);
+  const [total, setTotal] = useState(20);
   const [item, setItem] = useState(0);
   return (
-        <div className='App'>
-          <h2>Shopping Cart</h2>
-            <Navbar />
-            <Items />
-        </div>
+        
+          <itemContext.Provider value = {{total,setTotal}}>
+            <div className='App'>
+            <h2>Shopping Cart</h2>
+             <Navbar />
+             <Items />
+             </div>
+          </itemContext.Provider>
+            
+        
   );
 }
 export default App;
